ac250f882a6dc67cbc900af3162c89cb8c36880d
Eroiz <eric.roiz@wpengine.com>
2022-03-17 13:31:21 -0500

    headers test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ const { withFaust } = require('@faustjs/next');
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust();
+module.exports = { withFaust,   async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
+};
